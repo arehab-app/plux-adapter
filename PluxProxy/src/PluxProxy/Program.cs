e1156179f1093d9038fadc1c0e94024e35cb025c
zintiserics <zintis.erics@outlook.com>
2020-11-10 19:35:17 +0200

    Added NLog.

--- PluxProxy/src/PluxProxy/Program.cs
+++ PluxProxy/src/PluxProxy/Program.cs
@@ -1,12 +1,16 @@
-﻿using Console = System.Console;
+﻿using Logger = NLog.Logger;
+using LogManager = NLog.LogManager;
 
 namespace PluxProxy
 {
     public class Program
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("TODO");
+            logger.Debug("TODO");
+            LogManager.Shutdown();
         }
     }
 }
