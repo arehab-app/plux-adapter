a2fa55ce05275db104172967f44c8882fbeca2f0
zintiserics <zintis.erics@outlook.com>
2020-11-12 19:22:07 +0200

    Added CommandLineParser.

--- PluxProxy/src/PluxProxy/Program.cs
+++ PluxProxy/src/PluxProxy/Program.cs
@@ -1,16 +1,31 @@
-﻿using Logger = NLog.Logger;
-using LogManager = NLog.LogManager;
+﻿using System.Threading.Tasks;
+
+using NLog;
+using CommandLine;
 
 namespace PluxProxy
 {
-    public class Program
+    public static class Program
     {
+        private sealed class Options
+        {
+            [Option('t', "todo", Default="TODO", HelpText="TODO")]
+            public string TODO { get; set; }
+        }
+
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            logger.Debug("TODO");
+            int result = await Parser.Default.ParseArguments<Options>(args).MapResult(Execute, _ => Task.FromResult(1));
             LogManager.Shutdown();
+            return result;
+        }
+
+        private static async Task<int> Execute(Options options)
+        {
+            logger.Debug(options.TODO);
+            return 0;
         }
     }
 }
